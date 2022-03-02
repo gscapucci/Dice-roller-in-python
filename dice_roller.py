1657897848b984c4feb205373056ac1500046703
Guilherme Capucci <guilherme_capucci@outlook.com>
2022-03-02 17:09:35 -0300

    single quot string literal

--- dice_roller.py
+++ dice_roller.py
@@ -10,7 +10,7 @@ class DiceRoller :
         self._sum = 0
 
     def _print(self) -> None:
-        print(self._numbers, " = ", self._sum)
+        print(self._numbers, ' = ', self._sum)
         
     def _reset(self):
         self._numbers.clear()
@@ -18,9 +18,9 @@ class DiceRoller :
     
     def roll(self, number_of_dices: int, number_of_faces: int):
         if number_of_dices < 1:
-            raise Exception("Number of dices less than one.")
+            raise Exception('Number of dices less than one.')
         if number_of_faces < 1:
-            raise Exception("Number of faces less than one.")
+            raise Exception('Number of faces less than one.')
         
         for _ in range(number_of_dices):
             num = (random.randint(1, number_of_faces))
@@ -28,16 +28,16 @@ class DiceRoller :
             self._sum += num
         
         self.number_of_rolls += 1
-        print("Roll number {}({}D{}):".format(self.number_of_rolls, number_of_dices, number_of_faces))
+        print('Roll number {}({}D{}):'.format(self.number_of_rolls, number_of_dices, number_of_faces))
         self._print()
         self._reset()
 
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     argv = sys.argv[1:]
     if len(argv) < 2:
-        raise Exception("Usage: python " + sys.argv[0] + " <number of dices> <number of faces>")
+        raise Exception('Usage: python ' + sys.argv[0] + ' <number of dices> <number of faces>')
 
     roller = DiceRoller()
     roller.roll(int(argv[0]), int(argv[1]))
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from tracemalloc import stop
-import dice_roller
+from unittest.util import strclass
+import dice_roller as dr
 
 def main():
-    roller = dice_roller.DiceRoller()
+    roller = dr.DiceRoller()
+    
     while True:
-        print("Type a number of dices you want to roll and number of faces each dice has.")
-        dices = int(input("Dices:"))
-        faces = int(input("Faces:"))
+        print('Type a number of dices you want to roll and number of faces each dice has.')
+        dices = int(input('Dices:'))
+        faces = int(input('Faces:'))
         roller.roll(dices, faces)
-        continue_rolling = input("Continue")
+        continue_rolling = input('Continue(Y(Yes)/N(No))? ')
+        if continue_rolling == 'N' or continue_rolling == 'No':
+            break
+        elif continue_rolling == 'Y' or continue_rolling == 'Yes':
+            pass
+        else:
+            raise Exception("Input error, input must to be Y(Yes) or N(No)")
+
+if __name__ == "__main__":
+    main()
